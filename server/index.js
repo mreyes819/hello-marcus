7d4130f4f07a387c8f475c558c7a469a04979ad8
mttrys <mttrys@gmail.com>
2017-06-26 15:53:34 -0700

    Set up server to listen to heroku port

--- server/index.js
+++ server/index.js
@@ -1,32 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+const app = express()
 
-var app = express();
-
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
-
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
 app.get('/', function (req, res) {
-  res.send('hello world!');
-});
+  res.send('Hello World!')
+})
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
-});
-
+app.listen(process.env.PORT, function () {
+  console.log(`Server listening on port ${process.env.PORT}!`)
+})
