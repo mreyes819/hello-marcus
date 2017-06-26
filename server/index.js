fb9a76124268a943091c5bb73a19642154e9a3ec
mttrys <mttrys@gmail.com>
2017-06-26 16:39:33 -0700

    add semi-colons

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello from the Heroku Server!')
-})
+  res.send('Hello from the Heroku Server!');
+});
 
 app.listen(process.env.PORT, function () {
-  console.log(`Server listening on port ${process.env.PORT}!`)
-})
+  console.log(`Server listening on port ${process.env.PORT}!`);
+});
