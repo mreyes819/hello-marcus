3776236e61438c7bb6738e90470cf31f0b99f0cf
mttrys <mttrys@gmail.com>
2017-06-26 06:31:30 -0700

    Update to dynamic PORT

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!')
+app.listen(process.env.PORT, function () {
+  console.log(`Server listening on port ${process.env.PORT}!`)
 })
