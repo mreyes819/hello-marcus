26c713207f84bcd828f00702665283a3d7088361
Corey <cry@Coreys-MacBook-Air.local>
2017-06-27 16:19:33 -0700

    Use bodyparser

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = require('./router');
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.use('/', router);
