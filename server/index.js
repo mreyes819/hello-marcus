c429a253d78f7c6c0855dfa7fa511be59afe7774
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-06-27 18:42:36 -0700

    fix port and add pg dependency

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const router = require('./router');
 const bodyParser = require('body-parser');
-const words_db = require('../database-postgres/models/model_words.js')
+const words_db = require('../database-postgres/models/model_words.js');
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -11,6 +12,6 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.use('/', router);
 
-app.listen(process.env.PORT, function () {
+app.listen(port, function () {
   console.log(`Server listening on port ${process.env.PORT}!`);
 });
