649f69d2a6194e411ca9de18cc3bf5e9115afa1a
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-06-28 14:53:10 -0700

    add space

--- server/apis/cbot.js
+++ server/apis/cbot.js
@@ -5,6 +5,7 @@ const cbot = {
 
   getResponse: (input) => {
     let query = `https://www.cleverbot.com/getreply?key=${process.env.cbot_key}&input=${input}`;
+    
     return request(query)
             .then((data) => {
               return JSON.parse(data).output;
