0396f2192bba731e387b6322d42a01664df384ea
Corey <cry@Coreys-MacBook-Air.local>
2017-06-27 18:27:06 -0700

    Ask location on page load

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -11,6 +11,30 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    let success = (pos) => {
+      var crd = pos.coords;
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    };
+
+    let error = (err) => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+
   handleVoiceSubmit() {
     console.log('Handling voice...');
     //Make Houndify API call
