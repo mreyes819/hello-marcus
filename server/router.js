8ae1f61edd2b64f7a0652e5f5d73d041c5be298f
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-06-28 22:40:47 -0700

    hook up voice endpoint

--- server/router.js
+++ server/router.js
@@ -31,8 +31,9 @@ router.post('/saved', (req, res) => {
 });
 
 router.post('/voice', (req, res) => {
+  console.log(req.body);
   
-  fred()
+  fred(req.body.RawTranscription, JSON.stringify(req.body.location))
   .then((data) => {
     res.send(data);
   })
