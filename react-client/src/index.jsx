7bcad6ff15c38818f482f46b59b14636c3c5f65a
mttrys <mttrys@gmail.com>
2017-06-28 18:55:30 -0700

    enable speech to text. send to server.

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,14 +2,112 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
-import Utils from './utils.js'
+import Utils from './utils.js';
+// import fs from 'fs';
+// import dotenv from 'dotenv';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       replies: []
-    }
+    };
+    // dotenv.config();
+    this.requestInfo = {
+      ClientID: 'F9_-nNkqfW0GYzyg_h8i4w==',
+      UserID: "test_user",
+      Latitude: 37.388309,
+      Longitude: -121.973968
+    };
+
+    var jsonElet = document.getElementById("responseJSON");
+
+    this.myClient = new Houndify.HoundifyClient({
+
+      //Your Houndify Client ID
+      clientId: clientID,
+
+      //You need to create an endpoint on your server
+      //for handling the authentication.
+      //See SDK's server-side method HoundifyExpress.createAuthenticationHandler().
+      authURL: "/houndifyAuth",
+
+      //For testing environment you might want to authenticate on frontend without Node.js server.
+      //In that case you may pass in your Houndify Client Key instead of "authURL".
+      //clientKey: "YOUR_CLIENT_KEY",
+
+      //Enable Voice Activity Detection
+      //Default: true
+      enableVAD: true,
+
+      //You need to create an endpoint on your server
+      //for handling the authentication and proxying
+      //text search http requests to Houndify backend
+      //See SDK's server-side method HoundifyExpress.createTextProxyHandler().
+      textSearchProxy: {
+        url: "/textSearchProxy",
+        method: "POST",
+        // headers: {}
+        // ... More proxy options will be added as needed
+      },
+      onResponse: function(response, info) {
+        if (response.AllResults && response.AllResults[0] !== undefined) {
+          // Send voice result to server
+          $.ajax({
+            url: '/voiceResult',
+            method: 'POST',
+            contentType: 'application/json',
+            data: response.stringify(undefined, 2),
+            success: (data) => {
+              this.setState({
+                replies: data
+              });
+            },
+            error: (err) => {
+              console.log('err', err);
+            }
+          });
+        }
+      },
+
+      //Fires if error occurs during the request
+      onError: function(err, info) {
+        jsonElet.parentNode.hidden = true;
+        infoElet.value = JSON.stringify(info, undefined, 2);
+        infoElet.parentNode.hidden = false;
+        document.getElementById("voiceIcon").className = "unmute big icon";
+      },
+
+      //Fires every time backend sends a speech-to-text
+      //transcript of a voice query
+      //See https://houndify.com/reference/HoundPartialTranscript
+      onTranscriptionUpdate: function(trObj) {
+        var transcriptElt = document.getElementById("query");
+        transcriptElt.value = trObj.PartialTranscript;
+      },
+
+      //Fires after abort() method is called on search object
+      onAbort: function(info) {},
+
+      //Fires when start() metods is called on search object
+      onRecordingStarted: function() {
+        document.getElementById("voiceIcon").className = "selected radio icon big red";
+      },
+
+      //Fires when recording ends either after stop(), abort() or
+      //when server detects the end of query and responds
+      //(VAD: https://houndify.com/docs#voice-activity-detection)
+      onRecordingStopped: function() {
+        document.getElementById("voiceIcon").className = "unmute big icon";
+        document.getElementById("textSearchButton").disabled = false;
+        document.getElementById("query").readOnly = false;
+      },
+
+      //Fires every time new audio frame of recording is captured
+      onAudioFrame: function(frame) {}
+    });
   }
 
   componentDidMount() {
@@ -17,18 +115,27 @@ class App extends React.Component {
     Utils.location();
   }
 
+  startStopVoiceSearch() {
+    if (this.myClient.voiceSearch.isStreaming()) {
+      //stops streaming voice search requests, expects the final response from backend
+      this.myClient.voiceSearch.stop();
+    } else {
+      this.myClient.voiceSearch.startRecording(this.requestInfo);
+      //starts streaming of voice search requests to Houndify backend
+      document.getElementById("voiceIcon").className = "loading circle notched icon big";
+      document.getElementById("textSearchButton").disabled = true;
+      document.getElementById("query").readOnly = true;
+    }
+  }
 
-  handleVoiceSubmit() {
-    console.log('Handling voice...');
-    //Make Houndify API call
-
-    //Create obj to test if we can pass result to Node server
-    var obj = {test: "test"};
+  textQuery() {
+    var query = document.getElementById('query').value;
+    console.log(query);
     $.ajax({
-      url: '/voice',
+      url: '/textResult',
       method: 'POST',
       contentType: 'application/json',
-      data: JSON.stringify(obj),
+      data: JSON.stringify({query: query}),
       success: (data) => {
         this.setState({
           replies: data
@@ -43,7 +150,23 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <Search handleVoiceSubmit = {this.handleVoiceSubmit.bind(this)}/>
+        <div className="ui center aligned basic segment container">
+          <form id="form" className="ui form" action="javascript:void(0);">
+            <div className="ui action big labeled fluid input field">
+              <div className="ui icon basic label button" onClick= {this.startStopVoiceSearch.bind(this)}>
+                <i id="voiceIcon" className="unmute big icon"></i>
+              </div>
+              <input id="query" type="text" placeholder="Click on a microphone icon or type in your query" />
+              <button id="textSearchButton" className="ui icon button" onClick= {this.textQuery.bind(this)}>
+                 <i className="search big icon"></i>
+              </button>
+            </div>
+            <div className="ui field" hidden>
+              <label>Response object</label>
+              <textarea id="responseJSON"></textarea>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
--- server/index.js
+++ server/index.js
@@ -8,24 +8,79 @@ console.log('port inside index', process.env.PORT);
 const port = process.env.PORT;
 const apis = require('../server/apis/index.js');
 
+// ** install dependencies for houndify **
+var fs = require('fs');
+var https = require('https');
+var path = require('path');
+//"houndify" module contains both client-side ("HoundifyClient") and server-side ("HoundifyExpress") parts of SDK
+var Houndify = require('houndify');
+//parse arguments
 
 
+// currently installed
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 
-//how to use cleverbot
-	// apis.cbot.getResponse('who is barack obama')
-	// .then((data) => console.log(data));
-// apis.weather.getWeather('{"lat":"37.7837039","lon":"-122.4091297"}')
-// .then((data) => console.log(data));
-//how to make a database call
-	// model.words.getAllWords()
-	// .then((data) => console.log(data));
-
 app.use('/', router);
+
+
+
+//config file
+var configFile = 'config'; // config.json file for credentials.
+var config = require(path.join(__dirname, configFile));
+
+//express app
+var publicFolder = '../react-client/dist'; // Not sure why this is being done!!!!
+app.use(express.static(path.join(__dirname, publicFolder)));
+
+// authenticates requests
+app.get('/houndifyAuth', Houndify.HoundifyExpress.createAuthenticationHandler({
+  clientId:  config.clientId,
+  clientKey: config.clientKey
+}));
+
+//sends the request to Houndify backend with authentication headers
+app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
+
+app.post('/voiceResult', function(req, res) {
+  console.log(req.body);
+})
+
+app.post('/textResult', function(req, res) {
+  console.log(req.body);
+})
+// if (config.https) {
+
+//   //ssl credentials
+//   var privateKey = fs.readFileSync(config.sslKeyFile);
+//   var certificate = fs.readFileSync(config.sslCrtFile);
+//   var credentials = { key: privateKey, cert: certificate };
+
+//   //https server
+//   var httpsServer = https.createServer(credentials, app);
+//   httpsServer.listen(port, function() {
+//     console.log("HTTPS server running on port", port);
+//     console.log("Open https://localhost:" + port, "in the browser to view the Web SDK demo");
+//   });
+
+// } else {
+
+//   app.listen(port, function() {
+//     console.log("HTTP server running on port", port);
+//     console.log("Open http://localhost:" + port, "in the browser to view the Web SDK demo");
+//   });
+
+// }
+
+
+
+
+
+
+
 
 app.listen(port, function () {
   console.log(`Server listening on port ${process.env.PORT}!`);
