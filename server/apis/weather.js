b5ab99e66394465c5e15fd5cd5c3e7f4cc00491f
Corey <cry@Coreys-MacBook-Air.local>
2017-06-28 19:45:36 -0700

    Formatted object from Weather API

--- server/apis/weather.js
+++ server/apis/weather.js
@@ -1,11 +1,18 @@
 const request = require('request-promise');
+require('dotenv').config();
 
 const weather = {
 
-  getResponse: (loc) => {
-    let coordinates = JSON.parse(loc)
-    let options = {
-      uri: 'http://api.openweathermap.org/data/2.5/weather?lat=' + coordinates.lat + '&lon=' + coordinates.lon,
+  getResponse: (loc, placeStr) => {
+    if(!placeStr) {
+      let coordinates = JSON.parse(loc);
+      var query = 'http://api.openweathermap.org/data/2.5/weather?lat=' + coordinates.lat + '&lon=' + coordinates.lon;
+    } else {
+      query = 'http://api.openweathermap.org/data/2.5/weather?q=' + placeStr;
+    }
+    console.log(`${process.env.weather_api_key}`)
+    var options = {
+      uri: query,
       method: 'GET',
       headers: {
         'User-Agent': 'Request-Promise',
@@ -13,8 +20,19 @@ const weather = {
         'x-api-key': `${process.env.weather_api_key}`
       }
     };
-    return request(options);
+    request(options)
+    .then((data) => {
+      let apiResponse = {
+        type: 'widget',
+        api: 'weather',
+        data: JSON.parse(data)
+      };
+      console.log(apiResponse)
+      return apiResponse;
+    })
   }
 };
+
+weather.getResponse('{"lat":"37.7837039","lon":"-122.4091297"}', 'London');
 
 module.exports = weather;
