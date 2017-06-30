1e488520ebf64d2430df7144fa83ea7a41954a85
Corey <cry@Coreys-MacBook-Air.local>
2017-06-29 17:01:06 -0700

    Working Yelp API Call

--- server/apis/yelp.js
+++ server/apis/yelp.js
@@ -1,6 +1,39 @@
-const yelp = (query) => {
-  console.log('make a yelp api call');
-  return 'Call from Yelp API';
-}
+const request = require('request-promise');
+const fredTools = require('../fred/parseTools');
+require('dotenv').config();
+
+
+const yelp = {
+
+  getResponse: (loc, placeStr, originalStr) => {
+    var options = {
+      uri: `https://api.yelp.com/v3/businesses/search?term=food&longitude=${loc.lon}&latitude=${loc.lat}&sort_by=rating`,
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Hello-Marcus',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      auth: { 'bearer': `${process.env.yelp_token}` },
+      json: true
+    };
+
+    return request(options)
+    .then((yelpData) => {
+
+      let restaurantData = yelpData.businesses[0];
+
+      let response = fredTools.constructWeathertext(originalStr, restaurantData);
+      
+      let apiResponse = {
+        type: 'widget',
+        api: 'yelp',
+        text: 'not finalized yet',
+        data: restaurantData
+      };
+      return apiResponse;
+    });
+  }
+
+};
 
 module.exports = yelp;
